03d760922c977f48e3dbb834862ea6f7c8fe3a41
Kallu Kaalia <kunalmittal.km@outlook.com>
2025-06-20 13:33:11 +0530

    Layouts for Grid Added

--- packages/frontend/src/App.tsx
+++ packages/frontend/src/App.tsx
@@ -1,10 +1,43 @@
+// src/App.tsx
 
-const App = () => {
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+
+// Import Layouts
+import MinimalLayout from './layout/MinimalLayout';
+import AppLayout from './layout/AppLayout';
+
+// Import Pages
+import HomePage from "@/pages/HomePage";
+import BrowsePetsPage from "@/pages/BrowsePetsPage";
+
+
+function App() 
+{
   return (
-    <div>
-      <p>hello</p>
-    </div>
-  )
+    <Router>
+      <Routes>
+
+        {/* --- Route Group 1: Minimal Layout --- */}
+        {/* landing page, register, contact us */}
+        <Route element={<MinimalLayout />}>
+          <Route 
+            path="/" 
+            element={<HomePage />} 
+          />
+        </Route>
+
+        {/* --- Route Group 2: Main App Layout --- */}
+        {/* browse pets page */}
+        <Route element={<AppLayout />}>
+          <Route 
+            path="/browse" 
+            element={<BrowsePetsPage />} 
+          />
+        </Route>
+        
+      </Routes>
+    </Router>
+  );
 }
 
-export default App
+export default App;
